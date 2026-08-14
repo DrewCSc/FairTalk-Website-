3fd7c7549012c3830ebecfe37e5a521163d6882e
DrewCSc <Andrewcovington472@gmail.com>
2026-08-13 20:26:35 -0400

    modified:   js/main.js
    added window.addeventListener
    
    this made the loader disappear after the page is loaded.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// wait for the window content to load fully 
+window.addEventListener('DOMContentLoaded',() => {
+    
+    //create a gsap timeline instance
+    const timeline =gsap.timeline({
+        defaults: {ease: "power3.out" duration: 1}
+    })
+
+    //Animate the main heading
+     )
