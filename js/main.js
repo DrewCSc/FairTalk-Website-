156e9fee4730e2dcde2d6c285c067849b99feac4
DrewCSc <Andrewcovington472@gmail.com>
2026-08-18 15:06:36 -0400

    modified the about.html, adding the navigation bar.
    modified the about.css file to make the navigation bar appear like the homepage and link to the correct pages.
    
    including back to the homepage

--- js/main.js
+++ js/main.js
@@ -1,19 +1,4 @@
 
-// wait for the window content to load fully
-window.addEventListener('DOMContentLoaded', () => {
-
-    // create a gsap timeline instance (uses GSAP if available on the page)
-    const timeline = window.gsap?.timeline({
-        defaults: { duration: 0.75, ease: 'power1.out' }
-    });
-
-    // Animate paragraphs if GSAP loaded
-    if (timeline) {
-        // stagger will add a small delay between each `p` element
-        timeline.from('p', { y: 20, opacity: 0, stagger: 0.2 }, '-=0.6');
-    }
-
-});
 
 
 
